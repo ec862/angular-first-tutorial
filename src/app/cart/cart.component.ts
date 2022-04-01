f2cbdd99514efad4418989f4bc9611efce9a47e4
ec862 <11carringtonedw@gmail.com>
2022-04-01 11:50:54 +0100

    Managing Data Page

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -4,13 +4,15 @@ import { CartService } from '../cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-
   items = this.cartService.getItems();
 
-  constructor(
-    private cartService: CartService
-  ) { }
+  constructor(private cartService: CartService) {}
+
+  emptyCart() {
+    this.cartService.clearCart();
+    window.location.reload();
+  }
 }
